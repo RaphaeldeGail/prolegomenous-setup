e5dcabf9f0b218fd5c61ced961f2f8766ef1f952
Raphaël de Gail <raphael.de.gail@gmail.com>
2023-07-01 19:37:22 +0200

    Add first function in python script for project

--- run.py
+++ run.py
@@ -30,7 +30,14 @@ config_schema = Schema({
 ## the scopes for all google API calls
 scopes = ['https://www.googleapis.com/auth/cloud-platform']
 
-def create_project(parent, body):
+# set the clients for google API role and IAM policy
+# WARNING: the quota_project_id can not be set when calling the credentials
+# default() method and has to be explicitly overidden with
+# the with_quota_project(None) method.
+cred = default(scopes=scopes)[0].with_quota_project(None)
+project_api = build('cloudresourcemanager', 'v3', credentials=cred).projects()
+
+def create_project(body):
     """
     Create a project with google API call.
 
@@ -44,7 +51,56 @@ def create_project(parent, body):
     Raises:
         Exception: Raises an exception if the API call fails.
     """
-    return
+    return {}
+
+def set_project(parent, name='root'):
+    """
+    Set a project according to the project_data. Can either be create, update
+    or leave it as it is.
+
+    Args:
+        parent: string, the name of the organization in
+            the form organizations/{orgId}
+        name: string, the project display Name. Defaults to 'root'.
+
+    Returns:
+        dict, the result project.
+    """
+    print(
+        '[projects:{project}] setting up... '.format(project=name),
+        end=''
+    )
+    # this is the query to find the project
+    query = 'displayName={name} AND parent={parent} AND labels.root=true \
+AND labels.uuid:* AND projectId:{name}-*'.format(parent=parent, name=name)
+    request = project_api.search(query=query)
+    try:
+        result_project = request.execute()
+    except:
+        print('Error searching for projects.', end='')
+    if not 'projects' in result_project:
+        print('the project will be created... ', end='')
+        # random UUID for the project
+        uuid = 1234
+        body = {
+            "displayName": name,
+            "labels": {
+                "root": "true",
+                "uuid": uuid
+            },
+            "parent": parent,
+            "projectId": '{name}-{uuid}'.format(name=name, uuid=uuid),
+        }
+        result_project = create_project(body=body)
+        if result_project is None:
+            print('[ERROR] project creation failed')
+            return None
+        print('project successfully created.')
+        return result_project
+    else:
+        print('the project is already up-to-date.')
+        result_project = result_project['projects'][0]
+    return result_project
 
 # load the environment from setup.yaml
 with open('setup.yaml', 'r') as f:
@@ -54,3 +110,10 @@ try:
     config_schema.validate(setup)
 except SchemaError as se:
     raise se
+
+# the organization number
+org_id = setup['google']['organization']
+## the organization name as string 'organizations/{org_id}'
+parent = 'organizations/{org_id}'.format(org_id=org_id)
+
+set_project(parent=parent)
